c64f80c246a73ea776b8269f9a75cbcdcf3ff8e2
SuperInstance DocBot <docbot@superinstance.ai>
2026-06-06 05:00:46 +0000

    chore: add .gitignore, remove build artifacts from tracking

--- target/debug/deps/eisenstein_quantize-7de078dd0535104b.d
+++
@@ -1,8 +0,0 @@
-/home/ubuntu/.openclaw/workspace/eisenstein-quantize/target/debug/deps/eisenstein_quantize-7de078dd0535104b.d: src/lib.rs src/lattice.rs src/integer.rs src/error.rs
-
-/home/ubuntu/.openclaw/workspace/eisenstein-quantize/target/debug/deps/eisenstein_quantize-7de078dd0535104b: src/lib.rs src/lattice.rs src/integer.rs src/error.rs
-
-src/lib.rs:
-src/lattice.rs:
-src/integer.rs:
-src/error.rs:
--- target/debug/deps/num_rational-136726c6467af713.d
+++
@@ -1,8 +0,0 @@
-/home/ubuntu/.openclaw/workspace/eisenstein-quantize/target/debug/deps/num_rational-136726c6467af713.d: /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/home/ubuntu/.openclaw/workspace/eisenstein-quantize/target/debug/deps/libnum_rational-136726c6467af713.rlib: /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/home/ubuntu/.openclaw/workspace/eisenstein-quantize/target/debug/deps/libnum_rational-136726c6467af713.rmeta: /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs /home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs
-
-/home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/lib.rs:
-/home/ubuntu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/num-rational-0.4.2/src/pow.rs:
